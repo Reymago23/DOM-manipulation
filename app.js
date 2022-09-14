b2d8f90626e491c9ef5a0183e041bc065971d02f
Reymago23 <elderriqui@gmail.com>
2022-09-14 17:08:55 -0600

    add js

---
+++ app.js
@@ -0,0 +1,40 @@
+// window.addEventListener("click", function () {
+//     console.log('Window');
+// }, true);
+
+// document.addEventListener('click', function () {
+//     console.log('Document');
+// }, true);
+
+// document.querySelector('.div2').addEventListener("click", function () {
+//     console.log('DIV 2');
+// }, true);
+
+// document.querySelector('.div1').addEventListener("click", function () {
+//     console.log('DIV 1');
+// }, true);
+
+// document.querySelector('button').addEventListener("click", function (e) {
+//     console.log(e);
+// }, true);
+
+
+window.addEventListener("click", function () {
+    console.log('Window');
+}, false);
+
+document.addEventListener('click', function () {
+    console.log('Document');
+}, false);
+
+document.querySelector('.div2').addEventListener("click", function () {
+    console.log('DIV 2');
+}, false);
+
+document.querySelector('.div1').addEventListener("click", function () {
+    console.log('DIV 1');
+}, false);
+
+document.querySelector('button').addEventListener("click", function (e) {
+    console.log(e.target.innerText);
+}, false);
