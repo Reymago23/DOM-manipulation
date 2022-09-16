7427de000f99a0c8e79b60597cca89a42d62a0bf
Reymago23 <elderriqui@gmail.com>
2022-09-15 19:20:13 -0600

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'modal': 'rgba(0, 0, 0, .3)',
+      }
+    },
   },
   plugins: [],
 }
